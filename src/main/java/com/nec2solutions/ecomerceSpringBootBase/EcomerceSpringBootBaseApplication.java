1c6bb2dbcb8b2b7a1b3a499320150a7287ba9ccb
maddox000 <diegodacosta@baxen.com.ar>
2023-04-05 19:48:27 -0300

    Proyecto fundado desde Intializr

---
+++ src/main/java/com/nec2solutions/ecomerceSpringBootBase/EcomerceSpringBootBaseApplication.java
@@ -0,0 +1,13 @@
+package com.nec2solutions.ecomerceSpringBootBase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomerceSpringBootBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomerceSpringBootBaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nec2solutions/ecomerceSpringBootBase/EcomerceSpringBootBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nec2solutions.ecomerceSpringBootBase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomerceSpringBootBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
